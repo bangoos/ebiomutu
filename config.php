30dffc3bbca630b57bba7839f05edb34def53b0e
bangoos <80886547+bangoos@users.noreply.github.com>
2024-02-23 09:09:22 +0700

    first

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Configuration of the site */
+define('DATABASE_SERVER',   'localhost');
+define('DATABASE_USERNAME', 'u7624717_bangoos');
+define('DATABASE_PASSWORD', 'Mar11091995');
+define('DATABASE_NAME',     'u7624717_ebiomutu');
+define('SITE_URL',          'https://ebiomutu.site/');
